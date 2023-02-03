affb62f3ddff56f7dd483559cdda01c488862794
Shubham gupta <sgbhu01@gmail.com>
2023-02-03 13:31:58 +0530

    hello dosto

--- add.java
+++ add.java
@@ -2,9 +2,6 @@ public class add {
     public static void main(String[] args) {
         int a = 10, b = 50;
         System.out.println(a + b);
-        System.out.println(a + b);
-        System.out.println(a + b);
-        System.out.println(a + b);
 
     }
 }
